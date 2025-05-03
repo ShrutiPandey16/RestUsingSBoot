848f7cf380d95325194465a5c47d1b492a849a4e
shrutiPandey <shrutipandey23456@gmail.com>
2025-05-03 23:29:57 +0530

    Connected frontend-React and backend-springboot using CrossOrigin annotation

--- backend/spring-boot-rest/src/main/java/com/example/spring_boot_rest/controller/JobRestController.java
+++ backend/spring-boot-rest/src/main/java/com/example/spring_boot_rest/controller/JobRestController.java
@@ -3,15 +3,15 @@ package com.example.spring_boot_rest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.spring_boot_rest.model.JobPost;
 import com.example.spring_boot_rest.service.JobService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class JobRestController {
 	
 	@Autowired
