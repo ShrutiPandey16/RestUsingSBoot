83f0de0be90ded246800b1c09ea43b408c40f421
shrutiPandey <shrutipandey23456@gmail.com>
2025-05-03 23:06:53 +0530

    Added @RestController annotation

--- backend/spring-boot-rest/src/main/java/com/example/spring_boot_rest/controller/JobRestController.java
+++ backend/spring-boot-rest/src/main/java/com/example/spring_boot_rest/controller/JobRestController.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.spring_boot_rest.model.JobPost;
 import com.example.spring_boot_rest.service.JobService;
 
-@Controller
+@RestController
 public class JobRestController {
 	
 	@Autowired
--- backend/spring-boot-rest/src/main/java/com/example/spring_boot_rest/model/JobPost.java
+++ backend/spring-boot-rest/src/main/java/com/example/spring_boot_rest/model/JobPost.java
@@ -3,7 +3,6 @@ package com.example.spring_boot_rest.model;
 
 import java.util.List;
 
-import org.springframework.stereotype.Component;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,9 +10,14 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Component
 public class JobPost {
-
+//
+	private int postId;
+	private String postProfile; 
+	private String postDesc;
+	private int reqExperience;
+	private List<String> postTechStack;
+	
 	public JobPost(int postId, String postProfile, String postDesc, int reqExperience, List<String> postTechStack) {
 		// TODO Auto-generated constructor stub
 		this.postId = postId;
@@ -22,11 +26,51 @@ public class JobPost {
 		this.reqExperience = reqExperience;
 		this.postTechStack = postTechStack;
 	}
-	private int postId;
-	private String postProfile; 
-	private String postDesc;
-	private int reqExperience;
-	private List<String> postTechStack;
-	
 
+	public int getPostId() {
+		return postId;
+	}
+
+	public void setPostId(int postId) {
+		this.postId = postId;
+	}
+
+	public String getPostProfile() {
+		return postProfile;
+	}
+
+	public void setPostProfile(String postProfile) {
+		this.postProfile = postProfile;
+	}
+
+	public String getPostDesc() {
+		return postDesc;
+	}
+
+	public void setPostDesc(String postDesc) {
+		this.postDesc = postDesc;
+	}
+
+	public int getReqExperience() {
+		return reqExperience;
+	}
+
+	public void setReqExperience(int reqExperience) {
+		this.reqExperience = reqExperience;
+	}
+
+	public List<String> getPostTechStack() {
+		return postTechStack;
+	}
+
+	public void setPostTechStack(List<String> postTechStack) {
+		this.postTechStack = postTechStack;
+	}
+
+	@Override
+	public String toString() {
+		return "JobPost [postId=" + postId + ", postProfile=" + postProfile + ", postDesc=" + postDesc
+				+ ", reqExperience=" + reqExperience + ", postTechStack=" + postTechStack + "]";
+	}
+	
 }
